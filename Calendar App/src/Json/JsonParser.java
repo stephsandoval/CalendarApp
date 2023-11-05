c914d0aa2ff0dee5cc6d27468ee1adb1bf9c2d0f
Stephanie Sandoval <stephsandovalc@gmail.com>
2023-11-04 20:46:04 -0600

    screen fxmls and json parser

---
+++ Calendar App/src/Json/JsonParser.java
@@ -0,0 +1,73 @@
+package Json;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+
+public class JsonParser {
+    
+    private static JsonParser instance;
+    private JsonArray jsonWaterSource, jsonCropStatus, jsonPests, jsonAspects;
+    private ArrayList <String> waterSource, cropStatus, pests, aspects;
+
+    private JsonParser (){
+        loadJson ();
+        waterSource = toArray (jsonWaterSource);
+        cropStatus = toArray (jsonCropStatus);
+        pests = toArray (jsonPests);
+        aspects = toArray (jsonAspects);
+    }
+
+    public static synchronized JsonParser getInstance (){
+        if (instance == null){
+            instance = new JsonParser();
+        }
+        return instance;
+    }
+
+    private void loadJson (){
+        String filePath = "C:\\Users\\Stephanie\\OneDrive - Estudiantes ITCR\\Semestre II\\Programación Orientada a Objetos\\Caso 03\\Calendar App\\src\\Json\\config.json";
+        try (JsonReader reader = Json.createReader(new FileReader(filePath))){
+            JsonObject jsonParser = reader.readObject();
+
+            jsonWaterSource = jsonParser.getJsonArray("water source");
+            jsonCropStatus = jsonParser.getJsonArray("crop status");
+            jsonPests = jsonParser.getJsonArray("pests");
+            jsonAspects = jsonParser.getJsonArray("aspects");
+        } catch (Exception e){}
+    }
+
+    private ArrayList<String> toArray (JsonArray jsonArray){
+        if (jsonArray == null){
+            return null;
+        }
+        int beginning = 1;
+        ArrayList<String> array = new ArrayList<>();
+        for (JsonValue element : jsonArray){
+            String data = element.toString();
+            array.add(data.substring(beginning, data.length()-1));
+        }
+        return array;
+    }
+
+    public ArrayList<String> getWaterSource (){
+        return this.waterSource;
+    }
+
+    public ArrayList<String> getCropStatus (){
+        return this.cropStatus;
+    }
+
+    public ArrayList<String> getPests (){
+        return this.pests;
+    }
+
+    public ArrayList<String> getAspects (){
+        return this.aspects;
+    }
+}
